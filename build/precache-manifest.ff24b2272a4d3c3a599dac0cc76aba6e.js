07336140a2bf0669ba32530528f87cb678bd0a90
shagisayan <you@example.com>
2021-05-30 22:55:35 +0530

    upload final project

---
+++ build/precache-manifest.ff24b2272a4d3c3a599dac0cc76aba6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852f156484aa80207c64ee97987b75e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "415b68041525dc58b7d4",
+    "url": "/static/css/2.d1cfa505.chunk.css"
+  },
+  {
+    "revision": "fa8a88fbf27ae7d455e3",
+    "url": "/static/css/main.572abe4d.chunk.css"
+  },
+  {
+    "revision": "415b68041525dc58b7d4",
+    "url": "/static/js/2.94eb5a96.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.94eb5a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8a88fbf27ae7d455e3",
+    "url": "/static/js/main.a45b35e2.chunk.js"
+  },
+  {
+    "revision": "acf93c51f3611c015c1f",
+    "url": "/static/js/runtime-main.2e227a1f.js"
+  }
+]);
